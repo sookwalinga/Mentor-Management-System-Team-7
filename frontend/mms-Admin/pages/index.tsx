3daa1f9741d445f01c38cc5f6e91fba378e91702
ido evergeen <dicksonevegreenido@gmail.com>
2023-04-08 00:18:52 +0100

    feat: add setup file

---
+++ frontend/mms-Admin/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+var a;
+export default function Home() {
+  return (
+    <>
+      <h1> Home Page </h1>
+    </>
+  );
+}
