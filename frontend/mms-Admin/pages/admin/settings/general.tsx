c29d2ab2112a422c3c95e0186dc5a7f488026cf2
muwaffaq <itzmuwaffaq@gmail.com>
2023-04-15 11:40:35 +0100

    feat:implemented general settings form

--- frontend/mms-Admin/pages/admin/settings/general.tsx
+++ frontend/mms-Admin/pages/admin/settings/general.tsx
@@ -1,4 +1,6 @@
 import { Button, InputField, SelectField, TextareaField } from "@/components";
+import Input from "@/components/InputFields";
+import InputScaffold from "@/components/InputScaffold";
 import SettingsLayout from "@/components/Settings/SettingsLayout";
 import { DashboardLayout } from "@/components/layouts/dashboard-layout";
 import { avatarIcon } from "@/public";
@@ -24,69 +26,114 @@ const Profile = () => {
         </div>
         <div className="mt-5">
           <div className="form-group flex items-center justify-between">
-            <span className="font-semibold text-base text-mmsBlack2">Full Name</span>
+            <span className="font-semibold text-base text-mmsBlack2">
+              Full Name
+            </span>
             <div className="ml-6 flex items-center">
-            <InputField 
-            label=""
-            placeholder="First Name"
-            id="firstName"
-            type="text"
-            className="w-[326px]"
-            />
-            <InputField 
-            label=""
-            placeholder="Last Name"
-            id="lastName"
-            type="text"
-            className="md:w-[326px] ml-6"
-            />
+              <InputField
+                label=""
+                placeholder="First Name"
+                id="firstName"
+                type="text"
+                className="w-[326px]"
+              />
+              <InputField
+                label=""
+                placeholder="Last Name"
+                id="lastName"
+                type="text"
+                className="md:w-[326px] ml-6"
+              />
             </div>
           </div>
           <div className="form-group flex items-center justify-between">
-            <span className="font-semibold text-base text-mmsBlack2">About</span>
+            <span className="font-semibold text-base text-mmsBlack2">
+              About
+            </span>
             <div className="ml-6 flex items-center">
-            <TextareaField 
-            label=""
-            placeholder="Your Bio"
-            id="about"
-            className="md:w-[679px]"
-            type="text"
-            />
+              <TextareaField
+                label=""
+                placeholder="Your Bio"
+                id="about"
+                className="md:w-[679px]"
+                type="text"
+              />
             </div>
           </div>
           <div className="form-group flex items-center justify-between">
-            <span className="font-semibold text-base text-mmsBlack2">Full Website</span>
+            <span className="font-semibold text-base text-mmsBlack2">
+              Full Website
+            </span>
             <div className="ml-6 flex items-center">
-            <InputField 
-            label=""
-            placeholder="www.example.com"
-            id="website"
-            type="text"
-            className="md:w-[679px]"
-            />
+              <InputField
+                label=""
+                placeholder="www.example.com"
+                id="website"
+                type="text"
+                className="md:w-[679px]"
+              />
             </div>
           </div>
           <div className="form-group flex items-center">
             <div className="flex items-center">
-            <span className="font-semibold mr-20 text-base text-mmsBlack2">Country</span>
-            <SelectField 
-            label=""
-            id="country"
-            type="text"
-            className="w-[274px]"
-            >
-              <option value="">Select Country</option>
-            </SelectField>
+              <span className="font-semibold mr-[23%] text-base text-mmsBlack2">
+                Country
+              </span>
+              <SelectField
+                label=""
+                id="country"
+                type="text"
+                className="w-[294px]"
+              >
+                <option value="">Select Country</option>
+              </SelectField>
             </div>
-            <div className="ml-6 flex items-center">
-            <span className="font-semibold text-base text-mmsBlack2 mx-9">City </span>
-            <SelectField 
-            label=""
-            id="city"
-            className="w-[274px]"
-            >
-              <option value="">Select City</option>
-            </SelectField>
+            <div className="ml-[13%] flex items-center">
+              <span className="font-semibold text-base text-mmsBlack2 mr-9">
+                City{" "}
+              </span>
+              <SelectField label="" id="city" className="w-[294px]">
+                <option value="">Select City</option>
+              </SelectField>
+            </div>
+          </div>
+          <div className="form-group flex items-start">
+            <span className="font-semibold text-base text-mmsBlack2">
+              Social
+            </span>
+            <div className="ml-[11%] grid grid-cols-2 gap-x-8 gap-y-4 items-center">
+              <Input
+              prefix={<div className="flex items-center"> <Image src="/images/svgs/githubIcon.svg" height={24} width={24} alt="github"/> <span className="ml-2"> Github</span></div>}
+                label=""
+                placeholder="@githubuser"
+                id="githubUser"
+                type="text"
+                className="w-[294px]"
+              />
+              <Input
+              prefix={<div className="flex items-center"> <Image src="/images/svgs/instagramIcon.svg" height={24} width={24} alt="github"/> <span className="ml-2"> Github</span></div>}
+                label=""
+                placeholder="@githubuser"
+                id="githubUser"
+                type="text"
+                className="w-[294px] ml-8"
+              />
+              <Input
+              prefix={<div className="flex items-center"> <Image src="/images/svgs/linkedinIcon.svg" height={24} width={24} alt="github"/> <span className="ml-2"> Github</span></div>}
+                label=""
+                placeholder="@githubuser"
+                id="githubUser"
+                type="text"
+                className="w-[294px]"
+              />
+              <Input
+              prefix={<div className="flex items-center"> <Image src="/images/svgs/twitterIcon.svg" height={24} width={24} alt="github"/> <span className="ml-2"> Github</span></div>}
+                label=""
+                placeholder="@githubuser"
+                id="githubUser"
+                type="text"
+                className="w-[294px] ml-8"
+              />
             </div>
           </div>
         </div>
