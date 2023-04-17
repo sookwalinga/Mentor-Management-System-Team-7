6b48a780ae044b2953685d0f3eafbdeac79719c5
sam <sookwalinga@gmail.com>
2023-04-17 15:58:05 +0300

    feat: create error page

--- frontend/mms-Admin/public/index.ts
+++ frontend/mms-Admin/public/index.ts
@@ -1,25 +1,22 @@
-export { default as profileIcon} from './images/svgs/profile.svg'
-export { default as dashboardIcon} from './images/svgs/dashboard.svg'
-export { default as programsIcon} from './images/svgs/programs.svg'
-export { default as settingsIcon} from './images/svgs/settings.svg'
-export { default as certificateIcon } from './images/svgs/certificate.svg'
-export { default as searchIcon } from './images/svgs/search.svg'
-export { default as notificationIcon } from './images/svgs/notification.svg'
-export { default as chatIcon } from './images/svgs/chat.svg'
-export {default as avatarIcon } from './images/avatar.png'
-export {default as discussionIcon } from './images/svgs/discussion forum.svg'
-export {default as taskIcon } from './images/svgs/task.svg'
-export {default as reportIcon } from './images/svgs/reports.svg'
-export {default as mentorsIcon } from './images/svgs/Mentor.svg'
-export {default as mentorsManagerIcon } from './images/svgs/Mentor Managers.svg'
-export {default as approvalIcon } from './images/svgs/approval.svg'
-export {default as messagesIcon } from './images/svgs/messages.svg'
-export {default as mentorsIconLarge } from './images/svgs/Mentor ManagersLg.svg'
-export {default as taskIconLarge } from './images/svgs/taskLg.svg'
-export {default as reportIconLarge } from './images/svgs/reportsLg.svg'
-export {default as mentorsManagerIconLarge } from './images/svgs/Mentor ManagersLg.svg'
-export {default as GADSIcon } from './images/gads.png'
-
-
-
-
+export { default as profileIcon } from "./images/svgs/profile.svg";
+export { default as dashboardIcon } from "./images/svgs/dashboard.svg";
+export { default as programsIcon } from "./images/svgs/programs.svg";
+export { default as settingsIcon } from "./images/svgs/settings.svg";
+export { default as certificateIcon } from "./images/svgs/certificate.svg";
+export { default as searchIcon } from "./images/svgs/search.svg";
+export { default as notificationIcon } from "./images/svgs/notification.svg";
+export { default as chatIcon } from "./images/svgs/chat.svg";
+export { default as avatarIcon } from "./images/avatar.png";
+export { default as discussionIcon } from "./images/svgs/discussion forum.svg";
+export { default as taskIcon } from "./images/svgs/task.svg";
+export { default as reportIcon } from "./images/svgs/reports.svg";
+export { default as mentorsIcon } from "./images/svgs/Mentor.svg";
+export { default as mentorsManagerIcon } from "./images/svgs/Mentor Managers.svg";
+export { default as approvalIcon } from "./images/svgs/approval.svg";
+export { default as messagesIcon } from "./images/svgs/messages.svg";
+export { default as mentorsIconLarge } from "./images/svgs/Mentor ManagersLg.svg";
+export { default as taskIconLarge } from "./images/svgs/taskLg.svg";
+export { default as reportIconLarge } from "./images/svgs/reportsLg.svg";
+export { default as mentorsManagerIconLarge } from "./images/svgs/Mentor ManagersLg.svg";
+export { default as GADSIcon } from "./images/gads.png";
+export { default as errorPageIcon } from "./images/svgs/errorPage.svg";
